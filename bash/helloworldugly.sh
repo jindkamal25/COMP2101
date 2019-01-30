b2fd2d7fb980fca7fe64a76b2b3273ba0f93996e
kamaljeet kaur <kamal.kamaljeetchahal7@gmail.com>
2019-01-29 19:06:09 -0500

    overwrite the older one

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -1,10 +1,10 @@
-#!bin/bash
+#!/bin/bash
 # My third script - helloworldugly.sh
 # This script displays the string "Hello World!"
 
 # This is a silly way of creating the output text b/y starting with something else and stream editing it in a pipeline
 echo -n "helb wold" |
-  sed -e "s/b/o/g" -e "s/1/11" -e "s/o1/or1/" |
+  sed -e "s/b/o/g" -e "s/1/11/" -e "s/o1/or1/" |
   tr "h" "H"|
   tr "w" "W"|
   awk '{print $1 "\x20" $2 "\41"}'
