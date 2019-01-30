b03025e99c43c131fe0a905fac47b5b4422e0526
kamaljeet kaur <kamal.kamaljeetchahal7@gmail.com>
2019-01-29 19:03:59 -0500

    Lab 2 done having four scripts

---
+++ bash/arithmetic-demo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# this script demonstrates doing arithmetic
+
+# Improve this script by asking the user to supply the two numbers
+# Improve this script by demonstrating using subtraction and multiplication
+# Improve this script by demonstrating the modulus operation
+#   - the output should look something like:
+#   - first divided by second gives X with a remainder of Y
+# Improve this script by calculating and displaying the first number raised to the power of the second number
+
+firstnum=5
+secondnum=2
+sum=$((firstnum + secondnum))
+difference=$((firstnum - secondnum))
+product=$((firstnum * secondnum))
+remainder=$((firstnum % secondnum))
+dividend=$((firstnum / secondnum))
+fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+
+cat <<EOF
+$firstnum plus $secondnum is $sum
+$firstnum minus $secondnum is $difference
+$firstnum times $secondnum is $product
+$firstnum divided by $secondnum is $dividend, with the remainder of $remainder
+  - More precisely, it is $fpdividend
+EOF
---
+++ bash/diceroller.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# this script rolls a pair of six-sided dice
+#
+
+# Improve this script by adding a second line of output showing a roll of 5 six-sided dice
+# Improve this script by adding a third line of output that rolls a single 20-sided die
+# These improvements should also take into consideration the user, and make all 3 output lines self-explanatory to the user
+#    - e.g. the first line should be changed to read something like:
+#         Rolling a pair of standard dice: x,y rolled
+#      and the other output lines should be similarly helpful to the user
+
+# roll the dice and display the result
+echo "
+Rolling a pair of 6-sided dice....
+$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1 )) rolled
+"
+
+
+echo "
+Rolling a  5 6-sided dice....
+$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1 )) rolled
+"
+
+echo "
+One roll of a 20-sided die produced a $(( RANDOM % 20 + 1)),"
